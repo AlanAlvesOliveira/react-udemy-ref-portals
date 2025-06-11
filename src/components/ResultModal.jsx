76a45705e42d1e8400a8f562284e9ebb496e9137
Alan Alves <alan.oliveira@teledatabrasil.com.br>
2025-06-11 17:25:51 -0300

    useImperativeHandle (expor métodos internos do component com ref)

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,9 +1,24 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 //esse forwardRef é necessário para fazer funcionar em versões mais antigas do react
 const ResultModal= forwardRef(function ResultModal({ result, targetTime }, ref) {
+
+  const dialog =  useRef();
+
+  //serve para esconder os detalhes internos desse componente.
+  //ninguém deveria saber tem que usar esse showModal
+  //em vez disso: mostre o open()
+  
+  useImperativeHandle(ref, () => {
+    return {
+      open(){
+        dialog.current.showModal();
+      }
+    }
+  });
+
   return (
-    <dialog ref={ref} className="result-modal">
+    <dialog ref={dialog} className="result-modal">
       <h2>you {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds</strong>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
-  
+
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
 
@@ -12,8 +12,12 @@ export default function TimerChallenge({ title, targetTime }) {
   const handleStart = () => {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
-      dialog.current.showModal(); // essa funcionalidade é do html 5 e não do react
+      //dialog.current.showModal(); // essa funcionalidade é do html 5 e não do react
       //https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/dialog
+
+      //usando useImperativeHandle => serve para não conhecer os detalhes internos de ResultModal, 
+      //e como se tivesse "exponto" um metodo publico
+      dialog.current.open();
     }, targetTime * 1000);
     setTimerStarted(true);
   };
