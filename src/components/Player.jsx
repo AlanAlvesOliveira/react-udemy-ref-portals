f7dcc907a70ad418006edf20bd009fa3f33cdd75
Alan Alves <alan.oliveira@teledatabrasil.com.br>
2025-06-11 11:02:20 -0300

    Exemplo antes de usar refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
+
+  const [enteredPlayeName, setEnteredPlayerName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false)
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick(){
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayeName: 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input onChange={handleChange}  type="text" value={enteredPlayeName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
